41da869ec43b8f7db184bf297fbd4279d2f02e36
ASHER <ASHER@14.36.51.128>
2020-05-22 01:48:50 +0900

    *

--- src/main/webapp/resources/js/web/exam/ksk.js
+++ src/main/webapp/resources/js/web/exam/ksk.js
@@ -13,6 +13,10 @@
 			
 			initiallize :function(){
 				alert("김선규");
+				
+				$("#THE_HYK").onclick(function(){
+					
+				});
 			},
 			
 	};
@@ -22,3 +26,42 @@
 	})
 	
 })(); 
+
+
+
+
+
+
+
+
+/*	그냥 참조용임
+	// ajax 파일업로드 예시
+	var fromData = new Formdata($("#file-form")[0]);
+	
+	$.ajax({
+		type : "POST",
+		enctype: 'multipart/form-data',
+		url : "url/url.do",
+		data : formData,
+		processData : false,
+		contentType : false,
+		success : function(data){
+			
+		}, error : function(){
+			
+		}
+	});
+	
+	파일 다운로드
+	var xhr = new HMLHttpRequest();
+	xhr.open("POST", "url/url.do", true);
+	xhr.responseType = "blob";
+	xhr.onload = function(e){
+		var blob = xhr.response;
+		var link = document.createElemente('a');
+		link.href = window.URL.createObjectURL(blob);
+		link.download = "fileName";
+		link.click();
+	},bind(this)
+	xhr.send(JSON.stringify(param));
+*/	
