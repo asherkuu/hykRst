b03a81f38d84f86d64d2df6be5dde4073ac354b3
ASHER <ASHER@14.36.51.128>
2020-05-24 23:49:26 +0900

    *

--- src/main/webapp/resources/js/web/exam/ksk.js
+++ src/main/webapp/resources/js/web/exam/ksk.js
@@ -16,7 +16,7 @@
 			initiallize :function(){
 	        	$("#THE_HYK").on('click', function(e){
 	        		cmmn.util.Request.loadUrl("web/exam/ksk");
-	        		alert("에라이");
+	        		alert("에라이fdadfdfaadffad");
 	        	});
 			},
 			
